cb96334d8e23de4ac278305bd4d55136694bcf7e
anshulbahadure <anshul.bahadure123@gmail.com>
2022-03-24 12:57:19 +0530

    Calculation for Incometax-anshul

---
+++ JS/IncomeTaxCalculator.js
@@ -0,0 +1,28 @@
+
+    function myFunction() {
+      var age = document.getElementById("age").value
+      sessionStorage.setItem("age",age);
+
+      // sessionStorage.setItem("age", "age");
+      // let age = sessionStorage.getItem("age");
+      // document.getElementById("age").innerHTML = age;
+      
+      var city = document.getElementById("city").value
+      sessionStorage.setItem("city",city);
+      
+      var radios = document.getElementsByName('RecidenceS');
+      for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+   
+                var radio_value =radios[i].value;
+                       }
+                    }
+      sessionStorage.setItem("Resident Type",radio_value);
+      
+      
+      var occupation = document.getElementById("occupation").value
+      sessionStorage.setItem("occupation",occupation);
+
+      location.replace("SalaryDetails.html")
+     
+    }
