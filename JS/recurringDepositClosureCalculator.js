a11e9ecd67d4e6dc8c82b888d0664b9cd14c6aa3
shankar <shankarkadam54@gmail.com>
2022-03-27 09:43:05 +0530

    worked on income tax page - shankar

--- JS/recurringDepositClosureCalculator.js
+++ JS/recurringDepositClosureCalculator.js
@@ -9,9 +9,28 @@ $(function showGraphTable() {
   });
 });
 
-
+(function($) {
+  $.fn.inputFilter = function(inputFilter) {
+    return this.on("input keydown keyup mousedown mouseup select contextmenu drop", function() {
+      if (inputFilter(this.value)) {
+        this.oldValue = this.value;
+        this.oldSelectionStart = this.selectionStart;
+        this.oldSelectionEnd = this.selectionEnd;
+      } else if (this.hasOwnProperty("oldValue")) {
+        this.value = this.oldValue;
+        this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+      } else {
+        this.value = "";
+      }
+    });
+  };
+}(jQuery));
 
 $( document ).ready(function() {
+
+  $("input[type=text]").inputFilter(function(value) {
+    return /^\d*$/.test(value)  });
+
   //$("#penaltyRate").trigger( "change" );
   calculateRD();
 
